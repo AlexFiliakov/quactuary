409b442e3c416a0591a6b46474ffd5988733fe1a
Alex Filiakov <alexfiliakov@gmail.com>
2025-05-14 19:13:43 -0400

    package versioning backend

--- quactuary/__init__.py
+++ quactuary/__init__.py
@@ -1,1 +1,5 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
 from .backend import get_backend, set_backend
---
+++ quactuary/_version.py
@@ -0,0 +1,2 @@
+# Auto-generated version stub, overridden by setuptools_scm if available
+version = "0.0.0"

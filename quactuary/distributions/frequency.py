0defcdd4fe0b6cddf7ddd109ffb22e7660adc2df
Alex Filiakov <alexfiliakov@gmail.com>
2025-05-13 23:56:21 -0400

    added distributions

--- quactuary/distributions/frequency.py
+++ quactuary/distributions/frequency.py
@@ -1,3 +1,10 @@
+"""
+Frequency distributions for the number of claims per year.
+This module provides a set of frequency distributions that can be used to model the
+number of claims in a given period. The distributions are designed to be used with the
+quActuary library, which is a Python library for actuarial science and insurance modeling.
+"""
+
 from typing import Protocol
 
 
@@ -10,3 +17,39 @@ class FrequencyModel(Protocol):
 # - a ready‑made scipy.stats distribution,
 # - or a tiny helper like qa.freq.Poisson(mu=1.8) that just forwards to scipy.stats.poisson.
 # Internally we normalize everything to an adapter object that satisfies the protocol.
+
+
+class Binomial(FrequencyModel):
+    pass
+
+
+class FrequencyEmpirical(FrequencyModel):
+    pass
+
+
+class FrequencyMix(FrequencyModel):
+    pass
+
+
+class FrequencyTriangular(FrequencyModel):
+    pass
+
+
+class FrequencyUniform(FrequencyModel):
+    pass
+
+
+class Geometric(FrequencyModel):
+    pass
+
+
+class Hypergeometric(FrequencyModel):
+    pass
+
+
+class NegativeBinomial(FrequencyModel):
+    pass
+
+
+class Poisson(FrequencyModel):
+    pass
